3c4cc0c84e1dc8f3aa1af58acfc1a7c1393cdda5
Guido <guidodizi@gmail.com>
2020-02-18 09:39:51 -0300

    Refactor to class and jsdoc

--- src/index.js
+++ src/index.js
@@ -1,22 +1,37 @@
 import fnParams from 'get-parameter-names';
 
-export default () => {
-  const dependencies = {};
-  const factories = {};
-  const container = {};
+class Beaver {
+  constructor() {
+    this.dependencies = {};
+    this.factories = {};
+  }
 
-  container.factory = (name, factory) => {
+  /**
+   * @summary assign to a specific name a factory function that when executed will create an instance of dependency
+   * @param {string} name - unique name to register instances
+   * @param {Function} factory - factory function to create an instance of dependency
+   */
+  factory(name, factory) {
     factories[name] = factory;
-  };
+  }
 
-  container.register = (name, dependency) => {
+  /**
+   * @summary assign to a specific name an instance of a dependency. This can also be used to register constants i.e. beaver.register('TOKEN', 'secretToken!').
+   * @param {string} name - unique name to register instance.
+   * @param {*} dependency - instance of dependency.
+   */
+  register(name, dependency) {
     dependencies[name] = dependency;
-  };
+  }
 
-  container.get = (name) => {
+  /**
+   * @summary retrieve instance of dependency
+   * @param {string} name - identifier of dependency
+   */
+  get(name) {
     if (!dependencies[name]) {
       const factory = factories[name];
-      dependencies[name] = factory && container.inject(factory);
+      dependencies[name] = factory && this.inject(factory);
 
       if (!dependencies[name]) {
         throw new Error(`Cannot resolve dependecy ${name}`);
@@ -25,14 +40,18 @@ export default () => {
       return dependencies[name];
     }
     return dependencies[name];
-  };
+  }
 
-  container.inject = (factory) => {
+  /**
+   * @summary excecutes factory function to create instance of dependency.
+   * @param {Function} factory - factory function to instantiate dependency
+   */
+  _inject(factory) {
     // dependencies for selected factory
     const args = fnParams(factory).map((dep) => container.get(dep));
 
     return factory(...args);
-  };
+  }
+}
 
-  return container;
-};
+module.exports = () => new Beaver();
