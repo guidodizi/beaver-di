30596620bfcdcbeee0ab03814b136f6c4f1010a4
Guido <guidodizi@gmail.com>
2020-02-24 17:21:02 -0300

    do handle arrays as values

--- src/utils.js
+++ src/utils.js
@@ -3,11 +3,11 @@
 export function keying(obj, prefix = '') {
   return Object.keys(obj).reduce((acc, curr) => {
     if (Array.isArray(obj[curr])) {
-      throw new Error('Cannot define a dependency as an array');
-    } else if (typeof obj[curr] === 'object' && obj[curr] !== null) {
-      return [...acc, ...keying(obj[curr], `${prefix + curr}.`)];
-    } else {
       return [...acc, prefix + curr];
     }
+    if (typeof obj[curr] === 'object' && obj[curr] !== null) {
+      return [...acc, ...keying(obj[curr], `${prefix + curr}.`)];
+    }
+    return [...acc, prefix + curr];
   }, []);
 }
